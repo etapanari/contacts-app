8ed40442c9da4502b5007bf3761f909ad036f4b1
Electra Tapanari <etapanari@gmail.com>
2021-09-14 19:04:01 +0200

    added page to create new contact

--- app/controllers/concerns/contacts_controller.rb
+++ app/controllers/concerns/contacts_controller.rb
@@ -6,4 +6,18 @@ class ContactsController < ApplicationController
     def index
         @contacts = Contact.all
     end
+
+    def create
+        @contact = Contact.new(params.require(:contact).permit(:first_name, :last_name, :email, :phone_number))
+        if @contact.save 
+            flash[:notice] = "Contact was created successfully."
+            redirect_to contact_path(@contact)
+        else
+            render 'new'
+        end
+    end
+
+    def new
+        @contact = Contact.new
+    end
 end
