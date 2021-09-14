4be03f7876656c052fb8bf94936adfa1c04d01ea
Electra Tapanari <etapanari@gmail.com>
2021-09-14 23:03:51 +0200

    corrected typo in contacts page

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,7 +1,7 @@
 class Contact < ApplicationRecord
-    validates :first_name, presence: true , length: {minimum: 2 , maximum: 100}
-    validates :last_name, presence: true , length: {minimum: 2 , maximum: 100}
-    validates :email, presence: true , length: {minimum: 2 , maximum: 50}
-    validates :phone_number, presence: true , length: {minimum: 5 , maximum: 30}
+    validates :first_name, presence: true, length: {minimum: 2 , maximum: 100}
+    validates :last_name, presence: true, length: {minimum: 2 , maximum: 100}
+    validates :email, presence: true, email: true, length: {minimum: 2 , maximum: 50}
+    validates :phone_number, numericality: true, presence: true, length: {minimum: 5 , maximum: 30}
     validates_uniqueness_of :email
 end
