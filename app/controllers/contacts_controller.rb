81de73fe2cbe4cbc4799c6e8ed1ca93df177d418
Electra Tapanari <etapanari@gmail.com>
2021-09-16 14:17:00 +0200

    added comments

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,18 +2,32 @@ class ContactsController < ApplicationController
 
     before_action :set_contact, only: [:show, :edit, :update, :destroy, :changes]
 
+    
     def changes
-
-        number_of_audits = @contact.audits.length
-        @audits = @contact.audits.take(number_of_audits) 
-
+        
+        # These instance variables are set from the view dynamicaly as the view 
+        # iterates over the @changes hash and are used to display all fields of 
+        # 'History of edits' in the contacts/id/changes route
+        # This is done because the audits table only holds the editted fields 
+        # while I want to display all fields (changed and unchanged)
         @current_first_name=""
         @current_last_name=""
         @current_email=""
         @current_phone_number=""
         @current_timestamp=""
 
-        @all_changes = []
+        number_of_audits = @contact.audits.length
+        @audits = @contact.audits.take(number_of_audits) 
+
+        # @all_changes array will store all the hashes of each audit/edit.
+        # The edits are stored in the audited_changes column of the audits table
+        # as a hash with key being the column name(ie first_name) and value
+        # being a list with 2 elements. list[0] holds the value before edit
+        # and list[1] holds the value after edit. Special case is the
+        # initial creation of the record. It is also stored in the audited_changes
+        # column of the table but the value of the hash is a string.
+        # I also add to the @changes hash the created_at key-value pair
+        @all_changes = [] 
         (0..number_of_audits-1).each do | index |            
             @changes = @audits[index].audited_changes
             @changes["created_at"] = @audits[index].created_at
